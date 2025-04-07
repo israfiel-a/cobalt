ad12c9a435640f11c2da81efacfd217de2aae42c
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-04-06 23:49:28 -0400

    fresh start moment

--- utilities/idt.asm
+++
@@ -1,8 +0,0 @@
-; idt.asm
-; since 0.0.3
-; This file provides the definition for the Interrupt Descriptor
-; table (IDT), which provides access to interrupts beyond 16-bit
-; Real Mode.
-;
-; Copyright (c) 2025 Israfil Argos
-; This source code is under the AGPLv3.
