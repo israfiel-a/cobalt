0e93a70048eb4cd44863d5c353adf35f0d563106
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-04-13 21:22:07 -0400

    documented build script and cleaned up output, removed unneeded newlines from license (they were pissing me off), merged image create shell script into main build script

--- Toolchain/CreateImage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd Cobalt
-dd if=/dev/zero of=$1.img bs=1k count=1440
-mformat -i $1.img -f 1440 ::
-mmd -i $1.img ::/EFI
-mmd -i $1.img ::/EFI/BOOT
-mcopy -i $1.img BOOTX64.efi ::/EFI/BOOT
-echo Created FAT image.
