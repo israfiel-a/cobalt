310268f2f30a1c9ec3ece0446d0c555304483be7
Israfil <174603598+israfiel-a@users.noreply.github.com>
2025-04-17 20:30:49 -0400

    removed firmware wait prompt from waitKey

--- Source/Boot.c
+++ Source/Boot.c
@@ -20,36 +20,34 @@
 COBALT_OUTPUT_STREAM cobalt_conOut = nullptr;
 COBALT_INPUT_STREAM cobalt_conIn = nullptr;
 
-static EFI_STATUS waitKey(uint64_t timeOut, EFI_SYSTEM_TABLE *table)
+static EFI_STATUS waitKey(uint8_t timeOut, EFI_BOOT_SERVICES *services)
 {
     EFI_STATUS resetStatus = cobalt_conIn->Reset(cobalt_conIn, false);
     if (EFI_ERROR(resetStatus)) return resetStatus;
 
-    Cobalt_PrimitivePrintf(
-        L"Exit into firmware with 'f'. Continuing in %U seconds.",
-        timeOut);
-
     EFI_EVENT timerEvent;
-    table->BootServices->CreateEvent(EVT_TIMER, TPL_APPLICATION, nullptr,
-                                     nullptr, &timerEvent);
-    table->BootServices->SetTimer(timerEvent, 1, 10000000);
+    services->CreateEvent(EVT_TIMER, TPL_APPLICATION, nullptr, nullptr,
+                          &timerEvent);
+    services->SetTimer(timerEvent, 1, 10000000);
 
     EFI_INPUT_KEY key;
     EFI_STATUS keyGrabStatus;
     for (uint64_t timeOutSpent = 0; timeOutSpent < timeOut; timeOutSpent++)
     {
         UINTN index;
-        table->BootServices->WaitForEvent(1, &timerEvent, &index);
+        services->WaitForEvent(1, &timerEvent, &index);
 
         keyGrabStatus = cobalt_conIn->ReadKeyStroke(cobalt_conIn, &key);
         if (keyGrabStatus == EFI_NOT_READY) continue;
 
         if (key.UnicodeChar == L'f')
         {
-            table->BootServices->SetTimer(timerEvent, 0, 0);
+            services->SetTimer(timerEvent, 0, 0);
             return (EFI_STATUS)-8008;
         }
     }
+
+    services->SetTimer(timerEvent, 0, 0);
     return keyGrabStatus;
 }
 
@@ -81,7 +79,13 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE _ImageHandle,
     (void)Cobalt_PrimitivePuts(L"Hi! CobaltOS now booting. :) ");
     (void)Cobalt_PrimitiveTimestamp(L"Boot Time: ");
 
-    EFI_STATUS keyStatus = waitKey(5, SystemTable);
+    uint8_t firmwareTimeout = 5;
+    Cobalt_PrimitivePrintf(
+        L"Exit into firmware with 'f'. Continuing in %U seconds.",
+        firmwareTimeout);
+    EFI_STATUS keyStatus =
+        waitKey(firmwareTimeout, SystemTable->BootServices);
     if (keyStatus == (EFI_STATUS)-8008) return 0;
+
     return keyStatus;
 }
